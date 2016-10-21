c76ec030f4644fd3a67fa6a884705fd7454f68de
Ankur Agrawal <ankur.ag12@gmail.com>
2016-10-21 01:27:37 -0400

    Constant speed, heading error correction only

--- heuristic_control.py
+++ heuristic_control.py
@@ -2,12 +2,16 @@ import gym
 import numpy as np
 import cv2
 import math
-from scipy.spatial import Voronoi, voronoi_plot_2d
 import time
+import sys
 
 INDICATOR_HEIGHT_FACTOR = 1/8.0
 NUM_NODES_IN_PATH = 10
 SAFE_RADIUS = 50
+DES_TRAJ_NODE_IND_1 = 2
+DES_TRAJ_NODE_IND_2 = 3
+CONTROLLER_GAIN = 0.001
+TRACK_WIDTH_TO_WINDOW_WIDTH = 1/3.0
 
 env = gym.make('CarRacing-v0')
 env.reset()
@@ -44,24 +48,70 @@ def find_path_nodes(contour_track, width, height):
                 left_edge_points.append([x,y])
         else:
             right_edge_points.insert(0, [x,y])
-        last_point[:] = [x,y]
+        last_point[:] = [x, y]
 
-    num_nodes_in_path = max(1,min(len(left_edge_points), len(right_edge_points), NUM_NODES_IN_PATH))
+    num_nodes_in_path = max(1, min(len(left_edge_points), len(right_edge_points), NUM_NODES_IN_PATH))
     left_step = len(left_edge_points)/num_nodes_in_path
     right_step = len(right_edge_points)/num_nodes_in_path
+    print len(left_edge_points), len(right_edge_points)
 
-    left_nodes = np.array(left_edge_points[left_step-1::left_step])
-    right_nodes = np.array(right_edge_points[right_step-1::right_step])
+    left_nodes = left_edge_points[left_step-1::left_step]
+    right_nodes = right_edge_points[right_step-1::right_step]
+    mid_nodes = []
+    for i in xrange(num_nodes_in_path):
+        mid_nodes.append([(left_nodes[i][0] + right_nodes[i][0])/2, (left_nodes[i][1] + right_nodes[i][1])/2])
 
-    mid_nodes = (left_nodes + right_nodes)/2
-    #print "path nodes", path_nodes
-    #print "car center", car_x, car_y
-    return mid_nodes, left_edge_points, right_edge_points
+    # Sort path nodes such that they form continuous trajectory starting from the vehicle
+    # Since the x position of the vehicle is anchored at half of window width,
+    # the first point of the sorted nodes is the lowermost point in a thin window in the middle.
+    # After that, the consecutive points are found by the least distance criteria.
+    mid_nodes_sorted = np.empty([num_nodes_in_path,2],dtype=int)
+    y_max = 0
+    for i, [x, y] in enumerate(mid_nodes):
+        if (1 + TRACK_WIDTH_TO_WINDOW_WIDTH)*width/2 > x > (1 - TRACK_WIDTH_TO_WINDOW_WIDTH)*width/2 and y > y_max:
+            y_max = y
+            i_max = i
+    mid_nodes_sorted[0,:] = mid_nodes[i_max]
+    mid_nodes.pop(i_max)
+
+    sort_ind = 0
+    while mid_nodes:    # While the list is not empty
+        min_dist = width**2 + height**2     # Some high value
+        curr_node = mid_nodes_sorted[sort_ind, :]
+        for i, [x, y] in enumerate(mid_nodes):
+            dist = distance_point_to_point(curr_node, [x, y])
+            if dist < min_dist:
+                min_dist = dist
+                i_min = i
+        sort_ind += 1
+        mid_nodes_sorted[sort_ind,:] = mid_nodes[i_min]
+        mid_nodes.pop(i_min)
+
+    return mid_nodes_sorted, left_edge_points, right_edge_points
+
+
+def distance_point_from_line(line_p1, line_p2, p):
+    x1 = line_p1[0]
+    y1 = line_p1[1]
+    x2 = line_p2[0]
+    y2 = line_p2[1]
+    xp = p[0]
+    yp = p[1]
+    dist = float(abs((y2 - y1)*xp - (x2 - x1)*yp + x2*y1 - y2*x1))/math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
+    return dist
+
+
+def distance_point_to_point(p1, p2):
+    x1 = p1[0]
+    y1 = p1[1]
+    x2 = p2[0]
+    y2 = p2[1]
+    return math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
 
 
 def race():
 
-    for frame_i in range(400):
+    for frame_i in xrange(sys.maxint):
 
         env.render()
 
@@ -70,7 +120,6 @@ def race():
             observation, reward, done, info = env.step(np.array([0, 0, 0]))
             continue
 
-        observation, reward, done, info = env.step(np.array([0.0, 0.01, 0]))
         win_height, win_width = observation.shape[0:2]
 
         play_height = int((1-INDICATOR_HEIGHT_FACTOR) * win_height)
@@ -109,34 +158,58 @@ def race():
         car_cnt = contours[car_cnt_ind][:,0,:]
 
         (car_x, car_y), (_, _), car_angle = cv2.minAreaRect(car_cnt)
-        rect = cv2.minAreaRect(car_cnt)
+        car_cent = np.array([car_x, car_y])
         mask_track_copy = np.copy(mask_track)
         _, contours_track, _ = cv2.findContours(mask_track_copy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # TODO: Randomly selecting first contour. Need a better way to correctly identify the contour car is in
-        cnt_track = contours_track[0][:,0,:]
+
+        # Among all the grey colored contours, track is the one with maximum area
+        max_area = 0
+        for i, contour in enumerate(contours_track):
+            area = cv2.contourArea(contour)
+            if area > max_area:
+                max_area = area
+                i_max = i
+        cnt_track = contours_track[i_max][:, 0, :]
 
         t1 = time.time()
         path_nodes, track_left_lim, track_right_lim = find_path_nodes(cnt_track, play_width, play_height)
+
         # print "time to find path = ", time.time() - t1
 
         for i in xrange(len(path_nodes)):
-            grafitti[path_nodes[i,1], path_nodes[i,0], :] = [255,255,255]
+            grafitti[path_nodes[i, 1], path_nodes[i, 0], :] = [255, 255, 255]
 
         for [x,y] in track_left_lim:
-            grafitti[y, x, :] = [255,0,0]
+            grafitti[y, x, :] = [255, 0, 0]
 
         for [x,y] in track_right_lim:
-            grafitti[y, x, :] = [0,0,255]
+            grafitti[y, x, :] = [0, 0, 255]
 
         grafitti[car_y, car_x] = [0, 255, 0]
 
+        # TODO: Ignore nodes which are within specified radius from the car to avoid jitter in control
+        num_path_nodes = path_nodes.shape[0]
+        desired_traj = np.array([path_nodes[DES_TRAJ_NODE_IND_2, ], path_nodes[DES_TRAJ_NODE_IND_1, ]])
+        cv2.line(grafitti, (desired_traj[0,0], desired_traj[0,1]), (desired_traj[1,0], desired_traj[1,1]), (255, 255, 255), 1)
+        crosstrack_error = distance_point_from_line(desired_traj[0,:], desired_traj[1,:], car_cent)
+        heading_error = math.pi/2 + math.atan2(desired_traj[0,1] - desired_traj[1,1], desired_traj[0,0] - desired_traj[1,0])
+        #print "crosstrack error", crosstrack_error, "heading error", heading_error
+
+        # Speeding law
+        speed = 0.02
+
+        # Steering law
+        #steer = -heading_error + math.atan(CONTROLLER_GAIN * crosstrack_error / speed)
+        steer = heading_error
+
+        #print "steer", steer
         cv2.imshow('frame', grafitti)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+
+        observation, reward, done, info = env.step(np.array([steer, speed, 0]))
+
+        if (cv2.waitKey(1) & 0xFF == ord('q')) or done:
             #print "track edges", track_edges
             break
-
-
-    # TODO: Ignore nodes which are within specified radius from the car to avoid jitter in control
 
     cv2.destroyAllWindows()
 
